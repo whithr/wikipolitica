48af0bf192b79d41266f725a4e2e2bccce43ea68
Haley Whitman <97698963+whithr@users.noreply.github.com>
2025-01-22 23:26:32 -0800

    add supabase types

--- src/routes/countries.lazy.tsx
+++ src/routes/countries.lazy.tsx
@@ -1,18 +1,21 @@
 import { useState, useEffect } from 'react';
 import { createLazyFileRoute } from '@tanstack/react-router';
 import { createClient } from '@supabase/supabase-js';
+import { Database, Tables } from '@/lib/database.types';
 
 export const Route = createLazyFileRoute('/countries')({
   component: Country,
 });
 
-const supabase = createClient(
+const supabase = createClient<Database>(
   'https://syuiizinwoqmdgqziyyr.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5dWlpemlud29xbWRncXppeXlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2MTUwMTYsImV4cCI6MjA1MzE5MTAxNn0.QuFIM9-quiQv-Qtu25gisUYsDSYFcHRqMY8nnKrk_C8'
 );
 
 function Country() {
-  const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState<Tables<'countries'>[] | null>(
+    null
+  );
 
   useEffect(() => {
     getCountries();
@@ -20,14 +23,12 @@ function Country() {
 
   async function getCountries() {
     const { data } = await supabase.from('countries').select();
-    setCountries(data);
+    setCountries(data || null);
   }
 
   return (
     <ul>
-      {countries.map((country) => (
-        <li key={country.name}>{country.name}</li>
-      ))}
+      {countries?.map((country) => <li key={country.name}>{country.name}</li>)}
     </ul>
   );
 }
