918e7adc1561dae4e5d26a84374a7088999601cc
Haley Whitman <97698963+whithr@users.noreply.github.com>
2025-01-22 23:57:05 -0800

    fix tailwind install

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,23 +1,30 @@
+import { AppSidebar } from '@/components/AppSidebar';
+import { Button } from '@/components/ui/button';
+import { SidebarProvider } from '@/components/ui/sidebar';
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className='p-2 flex gap-2'>
-        <Link to='/' className='[&.active]:font-bold'>
-          Home
-        </Link>{' '}
-        <Link to='/about' className='[&.active]:font-bold'>
-          About
-        </Link>{' '}
-        <Link to='/countries' className='[&.active]:font-bold'>
-          Countries
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
-      <TanStackRouterDevtools />
+      <SidebarProvider>
+        {/* <div className='p-2 flex gap-2'>
+          <Link to='/' className='[&.active]:font-bold'>
+            Home
+          </Link>{' '}
+          <Link to='/about' className='[&.active]:font-bold'>
+            About
+          </Link>{' '}
+          <Link to='/countries' className='[&.active]:font-bold'>
+            Countries
+          </Link>
+        </div>
+        <hr />
+        <Button>Hello World</Button> */}
+        <AppSidebar />
+        <Outlet />
+        <TanStackRouterDevtools />
+      </SidebarProvider>
     </>
   ),
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [react(), TanStackRouterVite()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
