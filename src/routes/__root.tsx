22872561c46d13bb589707220edf04d36b3ef9ba
Haley Whitman <97698963+whithr@users.noreply.github.com>
2025-01-23 21:48:46 -0800

    initial app layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,30 +1,82 @@
-import { AppSidebar } from '@/components/AppSidebar';
+import { AppSidebar } from '@/components/app-sidebar';
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+} from '@/components/ui/breadcrumb';
 import { Button } from '@/components/ui/button';
-import { SidebarProvider } from '@/components/ui/sidebar';
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
+import { Separator } from '@radix-ui/react-separator';
+import { createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <SidebarProvider>
-        {/* <div className='p-2 flex gap-2'>
-          <Link to='/' className='[&.active]:font-bold'>
-            Home
-          </Link>{' '}
-          <Link to='/about' className='[&.active]:font-bold'>
-            About
-          </Link>{' '}
-          <Link to='/countries' className='[&.active]:font-bold'>
-            Countries
-          </Link>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
+          <div className='flex items-center gap-2 px-4'>
+            <SidebarTrigger className='-ml-1' />
+            <Separator orientation='vertical' className='mr-2 h-4' />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className='hidden md:block'>
+                  <BreadcrumbLink href='#'>
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className='hidden md:block' />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
+          <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
+            <div className='aspect-video rounded-xl bg-muted/50' />
+            <div className='aspect-video rounded-xl bg-muted/50' />
+            <div className='aspect-video rounded-xl bg-muted/50' />
+          </div>
+          <div className='min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min' />
         </div>
-        <hr />
-        <Button>Hello World</Button> */}
-        <AppSidebar />
-        <Outlet />
-        <TanStackRouterDevtools />
-      </SidebarProvider>
-    </>
+      </SidebarInset>
+      <TanStackRouterDevtools position='bottom-right' />
+    </SidebarProvider>
   ),
 });
+
+// export const Route = createRootRoute({
+//   component: () => (
+//     <>
+//       <SidebarProvider>
+//         {/* <div className='p-2 flex gap-2'>
+//           <Link to='/' className='[&.active]:font-bold'>
+//             Home
+//           </Link>{' '}
+//           <Link to='/about' className='[&.active]:font-bold'>
+//             About
+//           </Link>{' '}
+//           <Link to='/countries' className='[&.active]:font-bold'>
+//             Countries
+//           </Link>
+//         </div>
+//         <hr />
+//         <Button>Hello World</Button> */}
+//         <AppSidebar />
+//         <SidebarTrigger />
+//         <Outlet />
+//         <TanStackRouterDevtools />
+//       </SidebarProvider>
+//     </>
+//   ),
+// });
