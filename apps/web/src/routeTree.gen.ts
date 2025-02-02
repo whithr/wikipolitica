b268becad84f128008e90b42569bd6fbe9ae2876
Haley Whitman <97698963+whithr@users.noreply.github.com>
2025-02-01 16:06:39 -0800

    Update routeTree.gen.ts

--- apps/web/src/routeTree.gen.ts
+++ apps/web/src/routeTree.gen.ts
@@ -8,181 +8,181 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
 // Import Routes
 
-import { Route as rootRoute } from './routes/__root'
-import { Route as HomeOverviewImport } fr./routes/home/OverviewOverview'
-import { Route as ExecutivePresidentialScheduleImport } from './routes/executive/presidential-schedule'
-import { Route as ExecutivePresidentialActionsImport } from './routes/executive/presidential-actions'
+import { Route as rootRoute } from "./routes/__root";
+import { Route as HomeOverviewImport } from "./routes/home/OverviewOverview";
+import { Route as ExecutivePresidentialScheduleImport } from "./routes/executive/presidential-schedule";
+import { Route as ExecutivePresidentialActionsImport } from "./routes/executive/presidential-actions";
 
 // Create Virtual Routes
 
-const AboutLazyImport = createFileRoute('/about')()
-const IndexLazyImport = createFileRoute('/')()
+const AboutLazyImport = createFileRoute("/about")();
+const IndexLazyImport = createFileRoute("/")();
 const ExecutiveVicePresidentLazyImport = createFileRoute(
-  '/executive/vice-president',
-)()
+  "/executive/vice-president",
+)();
 
 // Create/Update Routes
 
 const AboutLazyRoute = AboutLazyImport.update({
-  id: '/about',
-  path: '/about',
+  id: "/about",
+  path: "/about",
   getParentRoute: () => rootRoute,
-} as any).lazy(() => import('./routes/about.lazy').then((d) => d.Route))
+} as any).lazy(() => import("./routes/about.lazy").then((d) => d.Route));
 
 const IndexLazyRoute = IndexLazyImport.update({
-  id: '/',
-  path: '/',
+  id: "/",
+  path: "/",
   getParentRoute: () => rootRoute,
-} as any).lazy(() => import('./routes/index.lazy').then((d) => d.Route))
+} as any).lazy(() => import("./routes/index.lazy").then((d) => d.Route));
 
 const ExecutiveVicePresidentLazyRoute = ExecutiveVicePresidentLazyImport.update(
   {
-    id: '/executive/vice-president',
-    path: '/executive/vice-president',
+    id: "/executive/vice-president",
+    path: "/executive/vice-president",
     getParentRoute: () => rootRoute,
   } as any,
 ).lazy(() =>
-  import('./routes/executive/vice-president.lazy').then((d) => d.Route),
-)
+  import("./routes/executive/vice-president.lazy").then((d) => d.Route)
+);
 
 const HomeOverviewRoute = HomeOverviewImport.update({
-  id: '/home/Overview',
-  path: '/home/Overview',
+  id: "/home/Overview",
+  path: "/home/Overview",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
-const ExecutivePresidentialScheduleRoute =
-  ExecutivePresidentialScheduleImport.update({
-    id: '/executive/presidential-schedule',
-    path: '/executive/presidential-schedule',
+const ExecutivePresidentialScheduleRoute = ExecutivePresidentialScheduleImport
+  .update({
+    id: "/executive/presidential-schedule",
+    path: "/executive/presidential-schedule",
     getParentRoute: () => rootRoute,
-  } as any)
+  } as any);
 
-const ExecutivePresidentialActionsRoute =
-  ExecutivePresidentialActionsImport.update({
-    id: '/executive/presidential-actions',
-    path: '/executive/presidential-actions',
+const ExecutivePresidentialActionsRoute = ExecutivePresidentialActionsImport
+  .update({
+    id: "/executive/presidential-actions",
+    path: "/executive/presidential-actions",
     getParentRoute: () => rootRoute,
-  } as any)
+  } as any);
 
 // Populate the FileRoutesByPath interface
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
-    '/': {
-      id: '/'
-      path: '/'
-      fullPath: '/'
-      preLoaderRoute: typeof IndexLazyImport
-      parentRoute: typeof rootRoute
-    }
-    '/about': {
-      id: '/about'
-      path: '/about'
-      fullPath: '/about'
-      preLoaderRoute: typeof AboutLazyImport
-      parentRoute: typeof rootRoute
-    }
-    '/executive/presidential-actions': {
-      id: '/executive/presidential-actions'
-      path: '/executive/presidential-actions'
-      fullPath: '/executive/presidential-actions'
-      preLoaderRoute: typeof ExecutivePresidentialActionsImport
-      parentRoute: typeof rootRoute
-    }
-    '/executive/presidential-schedule': {
-      id: '/executive/presidential-schedule'
-      path: '/executive/presidential-schedule'
-      fullPath: '/executive/presidential-schedule'
-      preLoaderRoute: typeof ExecutivePresidentialScheduleImport
-      parentRoute: typeof rootRoute
-    }
-    '/home/Overview': {
-      id: '/home/Overview'
-      path: '/home/Overview'
-      fullPath: '/home/Overview'
-      preLoaderRoute: typeof HomeOverviewImport
-      parentRoute: typeof rootRoute
-    }
-    '/executive/vice-president': {
-      id: '/executive/vice-president'
-      path: '/executive/vice-president'
-      fullPath: '/executive/vice-president'
-      preLoaderRoute: typeof ExecutiveVicePresidentLazyImport
-      parentRoute: typeof rootRoute
-    }
+    "/": {
+      id: "/";
+      path: "/";
+      fullPath: "/";
+      preLoaderRoute: typeof IndexLazyImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/about": {
+      id: "/about";
+      path: "/about";
+      fullPath: "/about";
+      preLoaderRoute: typeof AboutLazyImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/executive/presidential-actions": {
+      id: "/executive/presidential-actions";
+      path: "/executive/presidential-actions";
+      fullPath: "/executive/presidential-actions";
+      preLoaderRoute: typeof ExecutivePresidentialActionsImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/executive/presidential-schedule": {
+      id: "/executive/presidential-schedule";
+      path: "/executive/presidential-schedule";
+      fullPath: "/executive/presidential-schedule";
+      preLoaderRoute: typeof ExecutivePresidentialScheduleImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/home/Overview": {
+      id: "/home/Overview";
+      path: "/home/Overview";
+      fullPath: "/home/Overview";
+      preLoaderRoute: typeof HomeOverviewImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/executive/vice-president": {
+      id: "/executive/vice-president";
+      path: "/executive/vice-president";
+      fullPath: "/executive/vice-president";
+      preLoaderRoute: typeof ExecutiveVicePresidentLazyImport;
+      parentRoute: typeof rootRoute;
+    };
   }
 }
 
 // Create and export the route tree
 
 export interface FileRoutesByFullPath {
-  '/': typeof IndexLazyRoute
-  '/about': typeof AboutLazyRoute
-  '/executive/presidential-actions': typeof ExecutivePresidentialActionsRoute
-  '/executive/presidential-schedule': typeof ExecutivePresidentialScheduleRoute
-  '/home/Overview': typeof HomeOverviewRoute
-  '/executive/vice-president': typeof ExecutiveVicePresidentLazyRoute
+  "/": typeof IndexLazyRoute;
+  "/about": typeof AboutLazyRoute;
+  "/executive/presidential-actions": typeof ExecutivePresidentialActionsRoute;
+  "/executive/presidential-schedule": typeof ExecutivePresidentialScheduleRoute;
+  "/home/Overview": typeof HomeOverviewRoute;
+  "/executive/vice-president": typeof ExecutiveVicePresidentLazyRoute;
 }
 
 export interface FileRoutesByTo {
-  '/': typeof IndexLazyRoute
-  '/about': typeof AboutLazyRoute
-  '/executive/presidential-actions': typeof ExecutivePresidentialActionsRoute
-  '/executive/presidential-schedule': typeof ExecutivePresidentialScheduleRoute
-  '/home/Overview': typeof HomeOverviewRoute
-  '/executive/vice-president': typeof ExecutiveVicePresidentLazyRoute
+  "/": typeof IndexLazyRoute;
+  "/about": typeof AboutLazyRoute;
+  "/executive/presidential-actions": typeof ExecutivePresidentialActionsRoute;
+  "/executive/presidential-schedule": typeof ExecutivePresidentialScheduleRoute;
+  "/home/Overview": typeof HomeOverviewRoute;
+  "/executive/vice-president": typeof ExecutiveVicePresidentLazyRoute;
 }
 
 export interface FileRoutesById {
-  __root__: typeof rootRoute
-  '/': typeof IndexLazyRoute
-  '/about': typeof AboutLazyRoute
-  '/executive/presidential-actions': typeof ExecutivePresidentialActionsRoute
-  '/executive/presidential-schedule': typeof ExecutivePresidentialScheduleRoute
-  '/home/Overview': typeof HomeOverviewRoute
-  '/executive/vice-president': typeof ExecutiveVicePresidentLazyRoute
+  __root__: typeof rootRoute;
+  "/": typeof IndexLazyRoute;
+  "/about": typeof AboutLazyRoute;
+  "/executive/presidential-actions": typeof ExecutivePresidentialActionsRoute;
+  "/executive/presidential-schedule": typeof ExecutivePresidentialScheduleRoute;
+  "/home/Overview": typeof HomeOverviewRoute;
+  "/executive/vice-president": typeof ExecutiveVicePresidentLazyRoute;
 }
 
 export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath
+  fileRoutesByFullPath: FileRoutesByFullPath;
   fullPaths:
-    | '/'
-    | '/about'
-    | '/executive/presidential-actions'
-    | '/executive/presidential-schedule'
-    | '/home/Overview'
-    | '/executive/vice-president'
-  fileRoutesByTo: FileRoutesByTo
+    | "/"
+    | "/about"
+    | "/executive/presidential-actions"
+    | "/executive/presidential-schedule"
+    | "/home/Overview"
+    | "/executive/vice-president";
+  fileRoutesByTo: FileRoutesByTo;
   to:
-    | '/'
-    | '/about'
-    | '/executive/presidential-actions'
-    | '/executive/presidential-schedule'
-    | '/home/Overview'
-    | '/executive/vice-president'
+    | "/"
+    | "/about"
+    | "/executive/presidential-actions"
+    | "/executive/presidential-schedule"
+    | "/home/Overview"
+    | "/executive/vice-president";
   id:
-    | '__root__'
-    | '/'
-    | '/about'
-    | '/executive/presidential-actions'
-    | '/executive/presidential-schedule'
-    | '/home/Overview'
-    | '/executive/vice-president'
-  fileRoutesById: FileRoutesById
+    | "__root__"
+    | "/"
+    | "/about"
+    | "/executive/presidential-actions"
+    | "/executive/presidential-schedule"
+    | "/home/Overview"
+    | "/executive/vice-president";
+  fileRoutesById: FileRoutesById;
 }
 
 export interface RootRouteChildren {
-  IndexLazyRoute: typeof IndexLazyRoute
-  AboutLazyRoute: typeof AboutLazyRoute
-  ExecutivePresidentialActionsRoute: typeof ExecutivePresidentialActionsRoute
-  ExecutivePresidentialScheduleRoute: typeof ExecutivePresidentialScheduleRoute
-  HomeOverviewRoute: typeof HomeOverviewRoute
-  ExecutiveVicePresidentLazyRoute: typeof ExecutiveVicePresidentLazyRoute
+  IndexLazyRoute: typeof IndexLazyRoute;
+  AboutLazyRoute: typeof AboutLazyRoute;
+  ExecutivePresidentialActionsRoute: typeof ExecutivePresidentialActionsRoute;
+  ExecutivePresidentialScheduleRoute: typeof ExecutivePresidentialScheduleRoute;
+  HomeOverviewRoute: typeof HomeOverviewRoute;
+  ExecutiveVicePresidentLazyRoute: typeof ExecutiveVicePresidentLazyRoute;
 }
 
 const rootRouteChildren: RootRouteChildren = {
@@ -192,11 +192,11 @@ const rootRouteChildren: RootRouteChildren = {
   ExecutivePresidentialScheduleRoute: ExecutivePresidentialScheduleRoute,
   HomeOverviewRoute: HomeOverviewRoute,
   ExecutiveVicePresidentLazyRoute: ExecutiveVicePresidentLazyRoute,
-}
+};
 
 export const routeTree = rootRoute
   ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>()
+  ._addFileTypes<FileRouteTypes>();
 
 /* ROUTE_MANIFEST_START
 {
