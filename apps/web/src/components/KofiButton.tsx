fd979bdaefd28bd4a7262ccaa70fff3d33ec53fb
Haley Whitman <97698963+whithr@users.noreply.github.com>
2025-02-02 17:04:35 -0800

    switch to kofi external link for mobile

--- apps/web/src/components/KofiButton.tsx
+++ apps/web/src/components/KofiButton.tsx
@@ -1,16 +1,10 @@
 import { useState } from 'react'
 import { useMediaQuery } from '@/hooks/useMediaQuery'
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerTrigger,
-} from '@/components/ui/drawer'
 import { Button } from './ui/button'
 import { KofiModal } from './KofiModal'
 import kofiimg from '@/assets/Ko-fi Symbol.png'
-import { X } from 'lucide-react'
+import { ExternalLink } from './ExternalLink'
 
 export const KofiButton = ({ label }: { label?: string | React.ReactNode }) => {
   const [open, setOpen] = useState(false)
@@ -42,49 +36,75 @@ export const KofiButton = ({ label }: { label?: string | React.ReactNode }) => {
     )
   }
 
+  return label ? (
+    <ExternalLink
+      href='https://ko-fi.com/haleywhitman'
+      label={
+        <Button variant='primary' size='xs'>
+          {label}
+        </Button>
+      }
+    />
+  ) : (
+    <ExternalLink
+      href='https://ko-fi.com/haleywhitman'
+      label={
+        <Button variant='outline' size='icon'>
+          <img
+            src={kofiimg}
+            className='h-5 min-h-5 min-w-5 hover:scale-105'
+            alt='Ko-fi'
+          />
+        </Button>
+      }
+    />
+  )
+
   // Mobile (drawer) version:
   // Very picky because of the iframe.
-  return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
-        {label ? (
-          <Button variant='primary' size='xs'>
-            {label}
-          </Button>
-        ) : (
-          <Button variant='outline' size='icon'>
-            <img
-              src={kofiimg}
-              className='h-5 min-h-5 min-w-5 hover:scale-105'
-              alt='Ko-fi'
-            />
-          </Button>
-        )}
-      </DrawerTrigger>
+  // return (
+  //   <Drawer open={open} onOpenChange={setOpen}>
+  //     <DrawerTrigger asChild>
+  //       {label ? (
+  //         <Button variant='primary' size='xs'>
+  //           {label}
+  //         </Button>
+  //       ) : (
+  //         <Button variant='outline' size='icon'>
+  //           <img
+  //             src={kofiimg}
+  //             className='h-5 min-h-5 min-w-5 hover:scale-105'
+  //             alt='Ko-fi'
+  //           />
+  //         </Button>
+  //       )}
+  //     </DrawerTrigger>
 
-      {/* 
-        Instead of forcing a full height with "height", we use "maxHeight"
-        so the container grows as needed up to the available viewport height.
-        Adjust the 50px offset as needed (e.g. if you have headers/margins)
-      */}
-      <DrawerContent
-        style={{
-          maxHeight: '700px',
-          overflowY: 'auto',
-          border: 'none',
-        }}
-      >
-        <DrawerClose className='flex h-min self-end' asChild>
-          <Button
-            variant='outline'
-            className='absolute right-4 top-4 h-8 w-8'
-            size='icon'
-          >
-            <X className='h-8 w-8' />
-          </Button>
-        </DrawerClose>
-        <KofiModal isMobile={true} />
-      </DrawerContent>
-    </Drawer>
-  )
+  //     {/*
+  //       Instead of forcing a full height with "height", we use "maxHeight"
+  //       so the container grows as needed up to the available viewport height.
+  //       Adjust the 50px offset as needed (e.g. if you have headers/margins)
+  //     */}
+  //     <DrawerContent
+  //       style={{
+  //         maxHeight: '700px',
+  //         overflowY: 'auto',
+  //         border: 'none',
+  //       }}
+  //     >
+  //       <DrawerClose className='flex h-min self-end' asChild>
+  //         <Button
+  //           variant='outline'
+  //           className='absolute right-4 top-4 h-8 w-8'
+  //           size='icon'
+  //         >
+  //           <X className='h-8 w-8' />
+  //         </Button>
+  //       </DrawerClose>
+  //       <KofiModal isMobile={true} />
+  //     </DrawerContent>
+  //   </Drawer>
+  // )
 }
+
+//https://ko-fi.com/haleywhitman
